e4d27d57ff37400c86a8355ad7991bf114e2c9a1
Gideon <kwarteon08@gmail.com>
2025-10-08 14:30:32 +0000

    vv

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -198,7 +198,7 @@ export default function Products() {
             key={product.id}
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0 * filteredProducts.indexOf(product), duration: 0.4 }}
+            transition={{ delay: 0.1 * filteredProducts.indexOf(product), duration: 0.4 }}
           >
             <ProductCard {...product} />
           </motion.div>
