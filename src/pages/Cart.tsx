ce15d2c925fcfcd6848292bda4de4b9d8dd7fac0
Gideon <kwarteon08@gmail.com>
2025-10-09 11:12:13 +0000

    cart text removed

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -147,7 +147,7 @@ export default function Cart() {
       >
         <Header />
         <main className="flex-1 container mx-auto px-4 py-2">
-          <h1 className="font-heading font-semibold text-3xl md:text-4xl mb-6">Cart</h1>
+          {/* <h1 className="font-heading font-semibold text-3xl md:text-4xl mb-6">Cart</h1> */}
 
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-2">
             <div className="lg:col-span-2 space-y-2">
