f10c69dbe2e457dd5620c3842df79b923b74b210
Gideon <kwarteon08@gmail.com>
2025-12-02 00:21:35 +0000

    order

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -151,7 +151,7 @@ export default function Orders() {
       <Header />
 
       <main className="container mx-auto px-4 py-6 mt-10">
-        <h1 className="font-heading text-3xl font-bold mb-6">My Orders</h1>
+        <h1 className="font-heading text-center text-md font-semibold mb-6">MY ORDERS</h1>
 
         <div className="space-y-4">
           {orders.map((order) => (
